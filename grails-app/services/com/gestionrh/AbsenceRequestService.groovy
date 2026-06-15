51a343f1ab1fdc91e5114976a19018f67ed13fe4
👑Lord_Clxt👑 <127218888+LordClxt@users.noreply.github.com>
2026-06-15 11:08:21 +0000

    Enhance AbsenceRequestService with logging and comments
    Added logging and JavaDoc comments throughout the AbsenceRequestService methods.

--- grails-app/services/com/gestionrh/AbsenceRequestService.groovy
+++ grails-app/services/com/gestionrh/AbsenceRequestService.groovy
@@ -8,19 +8,23 @@ import grails.gorm.transactions.Transactional
 
 @Transactional
 class AbsenceRequestService {
-
+    // Utilise java doc pour les comments
     // Return number of rows in absence_request table
     Integer count(){
+        // Ajouter du logging(check slf4j)
         return AbsenceRequest.count()
     }
-
+    
+    // Utilise de la java doc pour les comments
     // Return absence requests list
     List<AbsenceRequestResponseRhDTO> getAll(Map params) {
+        // Ajouter du logging(check slf4j)
         return AbsenceRequest
                 .list(params)
                 .collect{ new AbsenceRequestResponseRhDTO(it) }
     }
-
+    
+    // Utilise java doc pour les comments
     // Return one absence request with his id
     AbsenceRequestResponseRhDTO getById(Long id) {
         // Get absence request with his id
@@ -32,15 +36,20 @@ class AbsenceRequestService {
         return new AbsenceRequestResponseRhDTO(AbsenceRequest.get(id))
     }
 
+    // Utilise java doc pour les comments
     // Return absence requests list of one employee
+    // Définis les paramètres dans le service
     List<AbsenceRequestResponseDTO> getAllByEmployee(User authenticatedUser, Map params = [:]) {
+        // Ajouter du logging(check slf4j)
         return AbsenceRequest
                 .findAllByEmployee(authenticatedUser, params)
                 .collect{ new AbsenceRequestResponseDTO(it) }
     }
 
+    // Utilise java doc pour les comments
     // Create absence request
     AbsenceRequestResponseDTO create(User authenticatedUser, AbsenceRequestRequestDTO userInput) {
+        // Ajouter du logging(check slf4j)
         // Instantiate absence request object
         AbsenceRequest absenceRequest = new AbsenceRequest(userInput.properties)
 
@@ -55,6 +64,8 @@ class AbsenceRequestService {
 
     // Update absence request
     void update(User authenticatedUser, Long id, AbsenceRequestRequestDTO userInput) {
+        // Ajouter du logging(check slf4j)
+        // Crée une methode get id qui contiendra aussi la validation
         // Get absence request with his id
         AbsenceRequest absenceRequest = AbsenceRequest.get(id)
 
@@ -68,8 +79,11 @@ class AbsenceRequestService {
         absenceRequest.save(failOnError: true)
     }
 
+    // Utilise java doc pour les comments
     // Delete absence request
     void deleteById(User authenticatedUser, Long id) {
+        // Ajouter du logging(check slf4j)
+         // Crée une methode get id qui contiendra aussi la validation
         // Get absence request with his id
         AbsenceRequest absenceRequest = AbsenceRequest.get(id)
 
@@ -80,8 +94,11 @@ class AbsenceRequestService {
         absenceRequest.delete(failOnError: true)
     }
 
+    // Utilise java doc pour les comments
     // Approve absence request
     void approved(User authenticatedUser, long id) {
+        // Ajouter du logging(check slf4j)
+         // Crée une methode get id qui contiendra aussi la validation
         // Get absence request with his id
         AbsenceRequest absenceRequest = AbsenceRequest.get(id)
 
@@ -97,8 +114,11 @@ class AbsenceRequestService {
         absenceRequest.save(failOnError: true)
     }
 
+    // Utilise java doc pour les comments
     // Reject absence request
     void rejected(User authenticatedUser, long id) {
+        // Ajouter du logging(check slf4j)
+         // Crée une methode get id qui contiendra aussi la validation
         // Get absence request with his id
         AbsenceRequest absenceRequest = AbsenceRequest.get(id)
 
